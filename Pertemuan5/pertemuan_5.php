aa4c227370f836aeab76036f3f881e4f49ce52a8
Fajar Ramdhani <2210631170090@student.unsika.ac.id>
2024-06-16 18:22:45 +0700

    Tugas_kampus_klik

---
+++ Pertemuan5/pertemuan_5.php
@@ -0,0 +1,27 @@
+<?php
+    //membuat variable
+    $nama_mahasiswa = "Nina";
+    $nama_kamu = "Fathir";
+    
+    if($nama_mahasiswa == "Nina"){
+        $jenis_kelamin = "Perempuan";
+    }else if($nama_kamu == "Fathir"){
+        $jenis_kelamin = "Laki - laki";
+    }else{
+        $jenis_kelamin = "??";
+    }
+
+    echo "Hallo ".$nama_mahasiswa." Selamat <br> datang, saya ".$nama_kamu." jenis kelamin kamu adalah ".$jenis_kelamin;
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pertemuan 5</title>
+</head>
+<body>
+    <br>
+        Calon Pacar saya ....
+</body>
+</html>
---
+++ Pertemuan7/pertemuan_7.php
@@ -0,0 +1,28 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pertemuan 7</title>
+</head>
+
+<body>
+    <form action="proses_pertemuan_7.php" method="POST">
+        NPM :
+        <input type="text" name="npm" value="" \>
+        <br>
+        NILAI :
+        <input type="text" name="nilai" value="" \>
+        <br>
+        ULANGI
+        <input type="text" name="ulangi" value="" \>
+        <br>
+        <button type="submit" name="proses_data">
+            PROSES DATA
+        </button>
+    </form>
+</body>
+
+</html>
